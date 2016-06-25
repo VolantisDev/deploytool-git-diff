8498942b22f633a65c308f60ba28124e05340f17
Ben McClure <ben.mcclure@gmail.com>
2016-06-25 15:17:53 -0500

    Simplify config, add some helper modules, require latest deploytool, version bump to 0.1.1

--- index.js
+++ index.js
@@ -4,5 +4,8 @@
 
 module.exports = {
   deploy: require('./lib/deploy'),
+  deployFiles: require('./lib/deploy-files'),
+  dirEmpty: require('./lib/dir-empty'),
+  getDirs: require('./lib/get-dirs'),
   getFiles: require('./lib/get-files')
 };
